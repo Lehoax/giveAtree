4cf3952a083b82f7ea595828591d4da8f255a623
Lehoax <leolair.pro@gmail.com>
2022-11-28 14:00:08 +0100

    log signin token

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -29,8 +29,8 @@ module.exports.signIn = async (req, res) => {
     const { email, password } = req.body
     try {
       const user = await UserModel.login(email, password);
-      console.log(email);
       const token = createToken(user._id);
+      console.log(token);
       res.cookie('jwt', token, { httpOnly: true, maxAge});
       res.status(200).json({ user: user._id})
     } catch (err){
